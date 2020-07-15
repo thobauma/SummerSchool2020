0004ba62ee754a09c927bc64bbd3f728ed452834
Thomas Baumann <thobauma@stundent.ethz.ch>
2020-07-15 14:42:58 +0200

    x_new to gpu

--- miniapp/cuda/main.cu
+++ miniapp/cuda/main.cu
@@ -162,7 +162,7 @@ int main(int argc, char* argv[])
     }
 
     // TODO : ensure that the gpu copy of x_new has the up to date values that were just created
-    x_new.update_host();
+    x_new.update_device();
 
     flops_bc = 0;
     flops_diff = 0;
