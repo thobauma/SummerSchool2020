225eff392d5b638f04eeb5019eb905bdc8e9ae89
Thomas Baumann <thobauma@stundent.ethz.ch>
2020-07-15 13:45:03 +0200

    braket

--- miniapp/cuda/data.h
+++ miniapp/cuda/data.h
@@ -116,7 +116,7 @@ class Field {
     // TODO : implement the body of update_host() and update_device()
     void update_host() {
         // TODO: copy contents of device_ptr_ to host_ptr_
-        auto status = cudaMemcpy(host_ptr_), device_ptr_, xdim_*ydim_*sizeof(double), cudaMemcpyDeviceToHost);
+        auto status = cudaMemcpy(host_ptr_, device_ptr_, xdim_*ydim_*sizeof(double), cudaMemcpyDeviceToHost);
         cuda_check_status(status);
     }
 
