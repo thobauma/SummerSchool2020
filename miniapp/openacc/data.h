92581bbb637f2f5189a775ea9a2270a3c9061075
Thomas Baumann <thobauma@stundent.ethz.ch>
2020-07-16 15:17:39 +0200

    ptr->ptr_

--- miniapp/openacc/data.h
+++ miniapp/openacc/data.h
@@ -100,12 +100,12 @@ class Field {
     /////////////////////////////////////////////////
     void update_host() {
         // TODO: Update the host copy of the data
-        #pragma acc data copyin(ptr[0:xdim_*ydim_])
+        #pragma acc data copyin(ptr_[0:xdim_*ydim_])
     }
 
     void update_device() {
         // TODO: Update the device copy of the data
-        #pragma acc data copyout(ptr[0:xdim_*ydim_])
+        #pragma acc data copyout(ptr_[0:xdim_*ydim_])
     }
 
     private:
